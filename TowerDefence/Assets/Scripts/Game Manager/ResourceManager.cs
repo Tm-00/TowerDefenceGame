b36cfa117a63d47c857aa335e126bc401c06df5e
Tm-00 <t.mitchell3@wlv.ac.uk>
2025-03-31 10:03:58 +0100

    implemented main menu

--- TowerDefence/Assets/Scripts/Game Manager/ResourceManager.cs
+++ TowerDefence/Assets/Scripts/Game Manager/ResourceManager.cs
@@ -9,8 +9,10 @@ public class ResourceManager : MonoBehaviour
     public  float currentResource;
 
     [Header("Cooldowns")] 
-    private float cooldown = 60;
-    private float currentCooldown;
+    private float totalResourceCooldownTimer = 60;
+    private float currentResourceCooldownTimer = 15;
+    private float totalResourceCooldown;
+    private float currentResourceCooldown;
     
     [Header("UI")] 
     public TextMeshProUGUI currentResourceAmount;
@@ -18,12 +20,14 @@ public class ResourceManager : MonoBehaviour
     private void Awake()
     {
         currentResource = totalResource;
-        currentCooldown = cooldown;
+        totalResourceCooldown = totalResourceCooldownTimer;
+        currentResourceCooldown = totalResourceCooldownTimer;
     }
 
     private void Update()
     {
         AddTotalResource();
+        AddCurrentResource();
     }
 
     public void SubtractResource(float unitCost)
@@ -32,21 +36,39 @@ public class ResourceManager : MonoBehaviour
         currentResource -= unitCost;
         currentResourceAmount.text = "Resources: " + currentResource + " / " + totalResource;
     }
-
+    
+    private void AddCurrentResource()
+    {
+        if (currentResourceCooldown <= 0)
+        {
+            currentResource = Mathf.Clamp(currentResource, 0, totalResource);
+            currentResource += 1;
+            if (currentResourceCooldownTimer > 5)
+            {
+                currentResourceCooldownTimer = Mathf.Clamp(currentResourceCooldownTimer, 5, 15);
+                totalResourceCooldownTimer -= 1;
+            }
+            currentResourceCooldown = currentResourceCooldownTimer;
+        }
+        currentResourceAmount.text = "Resources: " + currentResource + " / " + totalResource;
+        currentResourceCooldown -= Time.deltaTime;
+    }
+    
     private void AddTotalResource()
     {
-        if (currentCooldown <= 0)
+        if (totalResourceCooldown <= 0)
         {
             totalResource += 5;
 
-            if (cooldown > 30)
+            if (totalResourceCooldownTimer > 30)
             {
-                cooldown = Mathf.Clamp(cooldown, 30, 60);
-                cooldown -= 10;
+                totalResourceCooldownTimer = Mathf.Clamp(totalResourceCooldownTimer, 30, 60);
+                totalResourceCooldownTimer -= 10;
             }
-            currentCooldown = cooldown;
+            totalResourceCooldown = totalResourceCooldownTimer;
         }
-        currentCooldown -= Time.deltaTime;
+        currentResourceAmount.text = "Resources: " + currentResource + " / " + totalResource;
+        totalResourceCooldown -= Time.deltaTime;
     }
     
     //TODO implement Upgrades
